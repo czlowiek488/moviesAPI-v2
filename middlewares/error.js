927a88bb781d17ef54ed3083ce5dbb44872a582c
Your Name <you@example.com>
2018-08-15 10:07:33 +0100

    better Errors

--- middlewares/error.js
+++ middlewares/error.js
@@ -6,16 +6,16 @@ export default (err, req, res, next) => {
 
     switch(err.message) {
         case 'Movie not found':
-        res.status( 404 ).json({message: err.message})
-        break;
+            res.status( 404 ).json({message: err.message})
+            break;
         case 'Request sorting query is invalid':
         case 'Request body is not valid':
-        res.status( 400 ).json({message: err.message})
-        break;
+            res.status( 400 ).json({message: err.message})
+            break;
         case 'Third-party server error':
-        res.status( 503 ).json({message: err.message})
-        break;
+            res.status( 503 ).json({message: err.message})
+            break;
         default:
-        res.status( 500 ).json({message: 'Internal Server Error'})
+            res.status( 500 ).json({message: 'Internal Server Error'})
     }
 }
