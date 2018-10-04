6ef76f1cba4f50956bfe11c6b3a624ff83d919b8
Łukasz Klejszta <LukaszKlejszta@gmail.com>
2018-10-04 18:48:58 +0100

    add username and creationTime to comment

--- routes/comment.js
+++ routes/comment.js
@@ -8,20 +8,24 @@ import imdbID from '../schemas/imdbID'
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-    const {error} = Joi.validate(req.query, imdbID)
-    if(error) throw Error('Request body is not valid')
-    if(0 >= (await dbMovie.countDocuments({"imdbID": req.query.imdbID}))) throw Error('Movie not found')
-    const comments = await dbComment.find({"imdbID": req.query.imdbID}, 'text')
-    res.json(comments)
+  const { error } = Joi.validate(req.query, imdbID)
+  if (error) throw Error('Request body is not valid')
+  if (0 >= (await dbMovie.countDocuments({ imdbID: req.query.imdbID })))
+    throw Error('Movie not found')
+  const comments = await dbComment.find(
+    { imdbID: req.query.imdbID },
+    'text username createdAt'
+  )
+  res.json(comments)
 })
 
 router.post('/', async (req, res, next) => {
-    const {error} = Joi.validate(req.body, comment)
-    if(error) throw Error('Request body is not valid')
-    if(0 >= (await dbMovie.countDocuments({"imdbID": req.body.imdbID}))) throw Error('Movie not found')
-    res.json(req.body)
-    await dbComment.create(req.body)
+  const { error } = Joi.validate(req.body, comment)
+  if (error) throw Error('Request body is not valid')
+  if (0 >= (await dbMovie.countDocuments({ imdbID: req.body.imdbID })))
+    throw Error('Movie not found')
+  res.json(req.body)
+  await dbComment.create(req.body)
 })
 
 export default router
-
