5257b03bd7482d4f0001d8d3ecf7a572aaf0bbc9
Łukasz Klejszta <LukaszKlejszta@gmail.com>
2018-10-21 13:34:42 +0100

    new start script

--- startup/database.js
+++ startup/database.js
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose'
 
 export default () => {
-    mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, { useNewUrlParser: true })
+  mongoose.connect(
+    `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${
+      process.env.DB_HOST
+    }:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    { useNewUrlParser: true }
+  )
 }
